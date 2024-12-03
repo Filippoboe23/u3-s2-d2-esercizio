0d6ac086836ff25b93bc7d129fc9b1c946381032
Filippoboe23 <fil.boeilboa@gmail.com>
2024-12-03 17:02:51 +0100

    u3-s2-d2-esercizio

---
+++ src/components/CommentArea.jsx
@@ -0,0 +1,136 @@
+/*  import { Component } from "react";
+import CommentList from "./CommentList";
+import AddComment from "./AddComment";
+import Loading from "./Loading";
+import Error from "./Error";
+
+class CommentArea extends Component {
+  state = {
+    comments: [],
+    isLoading: false,
+    isError: false
+  };
+
+  // componentDidMount = async () => {
+  //   try {
+  //     let response = await fetch(
+  //       'https://striveschool-api.herokuapp.com/api/comments/' +
+  //         this.props.asin,
+  //       {
+  //         headers: {
+  //           Authorization:
+  //             'Bearer inserisci-qui-il-tuo-token',
+  //         },
+  //       }
+  //     )
+  //     console.log(response)
+  //     if (response.ok) {
+  //       let comments = await response.json()
+  //       this.setState({ comments: comments, isLoading: false, isError: false })
+  //     } else {
+  //       console.log('error')
+  //       this.setState({ isLoading: false, isError: true })
+  //     }
+  //   } catch (error) {
+  //     console.log(error)
+  //     this.setState({ isLoading: false, isError: true })
+  //   }
+  // }
+
+  componentDidUpdate = async (prevProps) => {
+    if (prevProps.asin !== this.props.asin) {
+      this.setState({
+        isLoading: true
+      });
+      try {
+        let response = await fetch("https://striveschool-api.herokuapp.com/api/comments/" + this.props.asin, {
+          headers: {
+            Authorization: "Bearer inserisci-qui-il-tuo-token"
+          }
+        });
+        console.log(response);
+        if (response.ok) {
+          let comments = await response.json();
+          this.setState({
+            comments: comments,
+            isLoading: false,
+            isError: false
+          });
+        } else {
+          this.setState({ isLoading: false, isError: true });
+        }
+      } catch (error) {
+        console.log(error);
+        this.setState({ isLoading: false, isError: true });
+      }
+    }
+  };
+
+  render() {
+    return (
+      <div className="text-center">
+        {this.state.isLoading && <Loading />}
+        {this.state.isError && <Error />}
+        <AddComment asin={this.props.asin} />
+        <CommentList commentsToShow={this.state.comments} />
+      </div>
+    );
+  }
+}
+
+export default CommentArea; */
+
+//TRASFORMAZIONE DEL CLASS COMPONENT IN HOOK FUNCTION
+
+import React, { useState, useEffect } from "react";
+import CommentList from "./CommentList";
+import AddComment from "./AddComment";
+import Loading from "./Loading";
+import Error from "./Error";
+
+const CommentArea = ({ asin }) => {
+  const [comments, setComments] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    const fetchComments = async () => {
+      if (!asin) return;
+
+      setIsLoading(true);
+      try {
+        const response = await fetch(`https://striveschool-api.herokuapp.com/api/comments/${asin}`, {
+          headers: {
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzM3MTJjMDhhZDEyOTAwMTU4NzZiZDYiLCJpYXQiOjE3MzMyNDE1ODgsImV4cCI6MTczNDQ1MTE4OH0.uEmRVb9CwCQMCluwTKRPTMK69bLy4reOiYVw9MhkGo4"
+          }
+        });
+        if (response.ok) {
+          const comments = await response.json();
+          setComments(comments);
+          setIsError(false);
+        } else {
+          setIsError(true);
+        }
+      } catch (error) {
+        console.error(error);
+        setIsError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchComments();
+  }, [asin]);
+
+  return (
+    <div className="text-center">
+      {isLoading && <Loading />}
+      {isError && <Error />}
+      <AddComment asin={asin} />
+      <CommentList commentsToShow={comments} />
+    </div>
+  );
+};
+
+export default CommentArea;
